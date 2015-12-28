97a49329453022feb9b6fa606c6dc76b88f78785
Jason Yao <jasony.edu@gmail.com>
2015-12-27 20:47:10 -0500

    Fixed osx nano settings

--- osx/setup.sh
+++ osx/setup.sh
@@ -19,3 +19,38 @@ then
 else
 	fail "OSX dependency installation failed"
 fi
+
+# Removes unix config files
+	user=$(who am i | awk '{print $1}')
+
+	# Gets rid of .bash_profile
+	profile_file="/Users/$user/.bashrc"
+	if [ -f "$profile_file" ];
+	then
+		info "Deleting unix config file .bashrc"
+		rm $home/../.bash_profile
+		success "Deleted unix config file .bashrc"
+	else
+		success ".bashrc was not found"
+	fi
+
+# Sets up correct nano settings
+	nanorc_unix_file="/home/$user/.nanorc_unix"
+	if [ -f "$nanorc_unix_file" ];
+	then
+		info "Deleting nano config file .nanorc_unix"
+                rm $home/../.nanorc_unix
+                success "Deleted osx config file .nanorc_unix"
+        else
+                info ".nanorc_unix was not found"
+        fi
+
+	nanorc_file="/home/$user/.nanorc"
+	if [ -f "$nanorc_file" ];
+	then
+		info ".nanorc is already set"
+	else
+		info "Symlinking nano configs"
+		ln -s /Users/$user/.nanorc_unix /Users/$user/.nanorc
+		success "Symlinked nanorc for unix"
+	fi
