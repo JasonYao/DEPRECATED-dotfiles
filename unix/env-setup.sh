68e90494f4be582b76916e8311748c187ed3c5c6
Jason Yao <jasony.edu@gmail.com>
2016-07-19 23:01:56 +0800

    Added pyenv virtualenv for unix

--- unix/env-setup.sh
+++ unix/env-setup.sh
@@ -9,8 +9,8 @@ if [[ $(which pyenv) == "" ]]; then
 	info "Developing Environments: Pyenv is not installed, installing now"
 
 	# Checks for cache directory
-	if [[ ! -d /home/$username/.dotfiles_cache ]]; then
-		mkdir /home/$username/.dotfiles_cache
+	if [[ ! -d /home/"$username"/.dotfiles_cache ]]; then
+		mkdir /home/"$username"/.dotfiles_cache
 	fi
 
 	# Checks for pyenv in the cache
@@ -19,7 +19,7 @@ if [[ $(which pyenv) == "" ]]; then
 	else
 		info "Developing Environments: Pyenv was not found in the cache, downloading now"
 
-		if wget https://github.com/yyuu/pyenv/archive/master.zip -O /home/$username/.dotfiles_cache/pyenv.zip -q --show-progress ; then
+		if wget https://github.com/yyuu/pyenv/archive/master.zip -O /home/"$username"/.dotfiles_cache/pyenv.zip -q --show-progress ; then
 			success "Developing Environments: Pyenv is now downloaded and cached"
 		else
 			fail "Developing Environments: Pyenv failed to download"
@@ -30,14 +30,14 @@ if [[ $(which pyenv) == "" ]]; then
 	info "Developing Environments: Checking pyenv directory"
 	if [[ -d /home/$username/.pyenv ]]; then
 		success "Developing Environments: Pyenv directory is already enabled, updating now"
-		if git -C /home/$username/.pyenv pull &> /dev/null; then
+		if git -C /home/"$username"/.pyenv pull &> /dev/null; then
 			success "Developing Environments: Pyenv is fully updated"
 		else
 			fail "Developing Environments: Pyenv failed to update"
 		fi
 	else
 		info "Developing Environments: Pyenv directory has not been extracted yet, extracting now"
-		if unzip -q /home/$username/.dotfiles_cache/pyenv.zip -d /home/$username && mv /home/$username/pyenv-master /home/$username/.pyenv ; then
+		if unzip -q /home/"$username"/.dotfiles_cache/pyenv.zip -d /home/"$username" && mv /home/"$username"/pyenv-master /home/"$username"/.pyenv ; then
 			success "Developing Environments: Pyenv directory is now extracted"
 		else
 			fail "Developing Environments: Pyenv failed to be extracted"
@@ -47,6 +47,22 @@ else
 	success "Pyenv: Environment is already installed"
 fi
 
+# Pyenv-virtualenv
+if [[ -d /home/$username/.pyenv/plugins/pyenv-virtualenv ]]; then
+	success "Pyenv-virtualenv: Plugin is already installed, updating now"
+	if git -C /home/"$username"/.pyenv/plugins/pyenv-virtualenv pull &> /dev/null ; then
+		success "Pyenv-virtualenv: Plugin is now updated"
+	else
+		fail "Pyenv-virtualenv: Plugin failed to update"
+	fi
+else
+	info "Pyenv-virtualenv: Plugin is not yet installed, installing now"
+	if git clone https://github.com/yyuu/pyenv-virtualenv.git /home/"$username"/.pyenv/plugins/pyenv-virtualenv &> /dev/null ; then
+		success "Pyenv-virtualenv: Plugin is now installed"
+	else
+		fail "Pyenv-virtualenv: Plugin failed to be installed"
+	fi
+fi
 
 # Rbenv
 if [[ $(which rbenv) == "" ]]; then
@@ -58,7 +74,7 @@ if [[ $(which rbenv) == "" ]]; then
 	else
 		info "Developing Environments: Rbenv was not found in the cache, downloading now"
 
-		if wget https://github.com/rbenv/rbenv/archive/master.zip -O /home/$username/.dotfiles_cache/rbenv.zip -q --show-progress ; then
+		if wget https://github.com/rbenv/rbenv/archive/master.zip -O /home/"$username"/.dotfiles_cache/rbenv.zip -q --show-progress ; then
 			success "Developing Environments: Rbenv is now downloaded and cached"
 		else
 			fail "Developing Environments: Rbenv failed to download"
@@ -69,14 +85,14 @@ if [[ $(which rbenv) == "" ]]; then
 	info "Developing Environments: Checking rbenv directory"
 	if [[ -d /home/$username/.rbenv ]]; then
 		success "Developing Environments: Rbenv directory is already enabled, updating now"
-		if git -C /home/$username/.rbenv pull &> /dev/null; then
+		if git -C /home/"$username"/.rbenv pull &> /dev/null; then
 			success "Developing Environments: Rbenv is fully updated"
 		else
 			fail "Developing Environments: Rbenv failed to update"
 		fi
 	else
 		info "Developing Environments: Rbenv directory has not been extracted yet, extracting now"
-		if unzip -q /home/$username/.dotfiles_cache/rbenv.zip -d /home/$username && mv /home/$username/rbenv-master /home/$username/.rbenv ; then
+		if unzip -q /home/"$username"/.dotfiles_cache/rbenv.zip -d /home/"$username" && mv /home/"$username"/rbenv-master /home/"$username"/.rbenv ; then
 			success "Developing Environments: Rbenv directory is now extracted"
 		else
 			fail "Developing Environments: Rbenv failed to be extracted"
@@ -89,7 +105,7 @@ if [[ $(which rbenv) == "" ]]; then
 		success "Developing Environments: [Optional] Rbenv pre-compilation is already complete"
 	else
 		info "Developing Environments: [Optional] Rbenv pre-compilation is not complete, compiling now"
-		if /home/$username/.rbenv/src/configure && make -C /home/$username/.rbenv/src > /dev/null ; then
+		if /home/"$username"/.rbenv/src/configure && make -C /home/"$username"/.rbenv/src > /dev/null ; then
 			success "Developing Environments: [Optional] Rbenv pre-compilation is now complete"
 		else
 			warn "Developing Environments: [Optional] Rbenv pre-compilation failed, but rbenv command should still work"
